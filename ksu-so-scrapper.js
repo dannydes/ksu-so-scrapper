8139a9f6f7abb1223be82ea11c31b8b7322a7367
Daniel <desiradaniel2007@hotmail.com>
2014-12-27 15:04:16 +0100

    some error handling

--- ksu-so-scrapper.js
+++ ksu-so-scrapper.js
@@ -10,6 +10,10 @@ request({
 		request({
 			uri: 'http://ksu.org.mt' + $g(this).attr('href')
 		}, function(error, response, body) {
+			if (error || response.statusCode !== 200) {
+				return;
+			}
+
 			console.log($g(org).text().substring(9));
 
 			var contactPersonIndex = body.indexOf('President:');
@@ -21,10 +25,12 @@ request({
 
 			var $ = cheerio.load(body);
 			
-			var email = $('p a').filter(function(index, a) {
-				return /mailto/.test($(a).attr('href'));
-			}).text();
-			console.log(email);
+			//Print email
+			var mailtoLinkIndex = body.indexOf('mailto:');
+			if (mailtoLinkIndex !== -1) {
+				mailtoLinkIndex += 7;
+				console.log(body.substring(mailtoLinkIndex, body.indexOf('"', mailtoLinkIndex)));
+			}
 
 			//Print website
 			console.log($('p > a[target=_blank]').attr('href'));
